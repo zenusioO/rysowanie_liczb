885938af529b327db25def2972f19487fcf52749
zenusioO <a.mathan83@gmail.com>
2020-10-29 18:26:40 +0100

    q

---
+++ program.py
@@ -0,0 +1,230 @@
+from moduly.modul import *
+import pyfiglet
+import time
+
+napis = pyfiglet.figlet_format("UWAGA")
+print(napis)
+print("program obsluguje tylko slowa: jeden, dwa, trzy, cztery, piec, szesc, siedem, osiem, dziewiec, zero. ")
+time.sleep(5)
+
+bob.penup()
+bob.goto(-400, 0)
+
+napis = pyfiglet.figlet_format("Rysowanie     Liczb")
+print(napis)
+time.sleep(2)
+
+
+print("podaj pierwsza liczbe do narysowania")
+zmienna = input()
+if(zmienna == "jeden"):
+    jeden()
+    bob.penup()
+    bob.goto(-200,0)
+
+if(zmienna == "dwa"):
+    dwa()
+    bob.penup()
+    bob.goto(-200,0)
+
+if(zmienna == "trzy"):
+    trzy()
+    bob.penup()
+    bob.goto(-200,0)
+
+if(zmienna == "cztery"):
+    cztery()
+    bob.penup()
+    bob.goto(-200,0)
+
+if (zmienna == "piec"):
+        piec()
+        bob.penup()
+        bob.goto(-200, 0)
+
+if(zmienna == "szesc"):
+    szesc()
+    bob.penup()
+    bob.goto(-200,0)
+
+if(zmienna == "siedem"):
+    siedem()
+    bob.penup()
+    bob.goto(-200,0)
+
+if(zmienna == "osiem"):
+    osiem()
+    bob.penup()
+    bob.goto(-200,0)
+
+if(zmienna == "dziewiec"):
+    dziewiec()
+    bob.penup()
+    bob.goto(-200,0)
+
+if(zmienna == "zero"):
+    zero()
+    bob.penup()
+    bob.goto(-200,0)
+
+print("podaj druga liczbe do narysowania")
+zmienna = input()
+if(zmienna == "jeden"):
+     jeden()
+     bob.penup()
+     bob.goto(0, 0)
+
+if(zmienna == "dwa"):
+    dwa()
+    bob.penup()
+    bob.goto(0, 0)
+
+if(zmienna == "trzy"):
+    trzy()
+    bob.penup()
+    bob.goto(0, 0)
+
+if(zmienna == "cztery"):
+    cztery()
+    bob.penup()
+    bob.goto(0,0)
+
+if(zmienna == "piec"):
+    piec()
+    bob.penup()
+    bob.goto(0,0)
+
+if(zmienna == "szesc"):
+    szesc()
+    bob.penup()
+    bob.goto(0,0)
+
+if(zmienna == "siedem"):
+    siedem()
+    bob.penup()
+    bob.goto(0,0)
+
+if(zmienna == "osiem"):
+    osiem()
+    bob.penup()
+    bob.goto(0,0)
+
+if(zmienna == "dziewiec"):
+    dziewiec()
+    bob.penup()
+    bob.goto(0,0)
+
+if(zmienna == "zero"):
+    zero()
+    bob.penup()
+    bob.goto(0,0)
+
+
+print("podaj trzecia liczbe do narysowania")
+zmienna = input()
+if(zmienna == "jeden"):
+     jeden()
+     bob.penup()
+     bob.goto(200, 0)
+
+if(zmienna == "dwa"):
+    dwa()
+    bob.penup()
+    bob.goto(200, 0)
+
+if(zmienna == "trzy"):
+    trzy()
+    bob.penup()
+    bob.goto(200, 0)
+
+if(zmienna == "cztery"):
+    cztery()
+    bob.penup()
+    bob.goto(200,0)
+
+if(zmienna == "piec"):
+    piec()
+    bob.penup()
+    bob.goto(200,0)
+
+if(zmienna == "szesc"):
+    szesc()
+    bob.penup()
+    bob.goto(200,0)
+
+if(zmienna == "siedem"):
+    siedem()
+    bob.penup()
+    bob.goto(200,0)
+
+if(zmienna == "osiem"):
+    osiem()
+    bob.penup()
+    bob.goto(200,0)
+
+if(zmienna == "dziewiec"):
+    dziewiec()
+    bob.penup()
+    bob.goto(200,0)
+
+if(zmienna == "zero"):
+    zero()
+    bob.penup()
+    bob.goto(200,0)
+
+print("podaj czwarta liczbe do narysowania")
+zmienna = input()
+if(zmienna == "jeden"):
+     jeden()
+     bob.penup()
+     bob.goto(400, 0)
+
+if(zmienna == "dwa"):
+    dwa()
+    bob.penup()
+    bob.goto(400, 0)
+
+if(zmienna == "trzy"):
+    trzy()
+    bob.penup()
+    bob.goto(400, 0)
+
+if(zmienna == "cztery"):
+    cztery()
+    bob.penup()
+    bob.goto(400,0)
+
+if(zmienna == "piec"):
+    piec()
+    bob.penup()
+    bob.goto(400,0)
+
+if(zmienna == "szesc"):
+    szesc()
+    bob.penup()
+    bob.goto(400,0)
+
+if(zmienna == "siedem"):
+    siedem()
+    bob.penup()
+    bob.goto(400,0)
+
+if(zmienna == "osiem"):
+    osiem()
+    bob.penup()
+    bob.goto(400,0)
+
+if(zmienna == "dziewiec"):
+    dziewiec()
+    bob.penup()
+    bob.goto(400,0)
+
+if(zmienna == "zero"):
+    zero()
+    bob.penup()
+    bob.goto(400,0)
+
+
+
+print("Aby zakonczyc program nacisnij dowolny klawisz")
+zmienna = input()
